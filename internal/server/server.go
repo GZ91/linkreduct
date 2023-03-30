c3f18d5e07e4a4904c061d4781d0d6589d7ddda5
Георгий Зырянов <gzyryanov@icloud.com>
2023-03-30 22:55:35 +0300

    Не правильно отправлял ответы в теле письма вместо хедера и не правильно формировал ответное письмо, я пытался записать заголовки после записи тела.

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"errors"
-	"fmt"
 	"github.com/GZ91/linkreduct/internal/config"
 	"github.com/GZ91/linkreduct/internal/database"
 	"io"
@@ -11,6 +10,8 @@ import (
 )
 
 var Config config.Config
+
+type Middleware func(http.Handler) http.Handler
 
 func Start(config config.Config) (err error) {
 	defer func() {
@@ -21,56 +22,50 @@ func Start(config config.Config) (err error) {
 	Config = config
 
 	mux := &http.ServeMux{}
-	mux.HandleFunc("/", defaultHandler)
+	mux.Handle("/", Conveyor(http.HandlerFunc(methodGet), methodPost))
 
 	return http.ListenAndServe(config.GetAddressServer(), mux)
 }
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		if err := methodGet(&w, r); err != nil {
-			w.Write([]byte(fmt.Sprintf("%v /t/n", err)))
-		}
-	case http.MethodPost:
-		if err := methodPost(&w, r); err != nil {
-			w.Write([]byte(fmt.Sprintf("%v /t/n", err)))
-		}
-	default:
-		w.WriteHeader(http.StatusBadRequest)
+func Conveyor(h http.Handler, middlewares ...Middleware) http.Handler {
+	for _, middleware := range middlewares {
+		h = middleware(h)
 	}
+	return h
 }
 
-func methodGet(w *http.ResponseWriter, r *http.Request) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = errors.New(r.(string))
+func methodPost(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != http.MethodPost {
+			next.ServeHTTP(w, r)
+			return
 		}
-	}()
+
+		link, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		id := database.AddUrl(string(link), Config)
+		bodyText := "http://" + Config.GetAddressServer() + "/" + id
+		w.Header().Add("Content-Type", "text/plain")
+		w.Header().Add("Content-Length", string(len(bodyText)))
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(bodyText))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func methodGet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 	id := strings.TrimPrefix(r.URL.Path, "/")
 	if link, ok := database.GetUrl(id); ok {
-		w.Write([]byte(link))
+		w.Header().Add("Location", link)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
 	}
-	return nil
-}
-
-func methodPost(w *http.ResponseWriter, r *http.Request) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = r.(error)
-		}
-	}()
-	link, err := io.ReadAll(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return err
-	}
-	id := database.AddUrl(string(link), Config)
-	bodyText := "http://" + Config.GetAddressServer() + "/" + id
-	w.Write([]byte(bodyText))
-	w.WriteHeader(http.StatusCreated)
-	return nil
 }
