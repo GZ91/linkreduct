3afe6f2238aff5caec42f54911532b4be9c9f4cd
Георгий Зырянов <gzyryanov@icloud.com>
2023-04-05 22:28:36 +0300

    добавил возможность указать адрес URL без порта

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"github.com/GZ91/linkreduct/internal/app"
 	"github.com/GZ91/linkreduct/internal/config"
+	"strings"
 )
 
 func main() {
@@ -16,6 +17,17 @@ func main() {
 func Configuration() *config.Config {
 	addressServer := flag.String("a", "localhost:8080", "Run Address server")
 	addressServerURL := flag.String("b", "http://localhost:8080/", "Address server for URL")
+
+	strAddress := strings.Split(*addressServerURL, ":")
+	port := strAddress[2]
+
+	if len(port) == 0 {
+		port = strings.Split(*addressServer, ":")[1]
+	}
+	if port[len(port)-1] != '/' {
+		port = port + "/"
+	}
+
 	flag.Parse()
-	return config.New(false, *addressServer, *addressServerURL, 10)
+	return config.New(false, *addressServer, strAddress[0]+":"+strAddress[1]+":"+port, 10)
 }
