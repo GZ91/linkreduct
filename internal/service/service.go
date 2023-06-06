926f09b5c8c5696ea0864a4d50cab1c50b70bbb4
Георгий Зырянов <gzyryanov@icloud.com>
2023-06-06 21:40:37 +0300

    поправил возврат коротких ссылок пользователя

--- internal/service/service.go
+++ internal/service/service.go
@@ -111,7 +111,15 @@ func (r *NodeService) Ping(ctx context.Context) error {
 }
 
 func (r *NodeService) GetURLsUser(ctx context.Context, userID string) ([]models.ReturnedStructURL, error) {
-	return r.db.GetLinksUser(ctx, userID)
+	addressServer := r.conf.GetAddressServerURL()
+	returnedStructURL, err := r.db.GetLinksUser(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+	for index, val := range returnedStructURL {
+		returnedStructURL[index].ShortURL = addressServer + val.ShortURL
+	}
+	return returnedStructURL, nil
 }
 
 func (r *NodeService) DeletedLinks(listURLs []string, userID string) {
